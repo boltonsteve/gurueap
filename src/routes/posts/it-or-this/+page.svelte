99e48000210cb15a03ad15c740a66bf3c90779f1
Steve Bolton <sgbolton@gmail.com>
2023-08-20 13:53:38 +0800

    Sixth commit

--- src/routes/posts/it-or-this/+page.svelte
+++ src/routes/posts/it-or-this/+page.svelte
@@ -1,5 +1,17 @@
-<script>
-    import ItOrThis from './itOrThis.svelte'
-</script>
-
-<ItOrThis />
+    <h1><b><i>It</i></b> or <b><i>this</i></b>?</h1>
+    <p class="ml-3 font-italic">The government has just removed fuel subsidies. <span class="font-weight-bold text-danger">It</span> means that the price of basic goods will surely go up.</p>
+    <p>It's sometimes useful to think of a sentence as having a <b>theme</b> and a <b>rheme</b>:</p>
+    <div class="row-col" style="grid-template-columns: 1fr 2fr;">
+        <div class="font-weight-bold">Theme</div>
+        <div class="font-weight-bold">Rheme</div>
+        <div class="font-italic">The government..</div>
+        <div class="font-italic">..has just removed fuel subsidies.</div>
+    </div>
+    <p>When you want to refer back to the <b>theme</b>, use a <span class="underline">pronoun</span>:</p>
+    <p class="ml-3 font-italic">The government has just removed fuel subsidies. <span class="font-weight-bold text-success">They</span> felt that the fuel subsidies were not economically sustainable.</p>
+    <p>When you want to refer back to the <b>rheme</b>, use <span class="underline">this</span> or <span class="underline">these</span>:</p>
+    <p class="ml-3 font-italic">The government has just removed fuel subsidies. <span class="font-weight-bold text-success">This</span> means that the price of basic goods will surely go up.</p>
+    <hr>
+    <h3 class="text-info">Exam tip</h3>
+    <p>Choosing the right referencing word (<b><i>it</i></b> or <b><i>this</i></b>) will make your writing more coherent (easier to understand).</p>
+    <p>If you are preparing for IELTS, the right choice of referencing word will give you a higher score for coherence and cohesion (see IELTS public band descriptors for <a href="https://takeielts.britishcouncil.org/sites/default/files/ielts_writing_band_descriptors.pdf" target="_blank" rel="noopener">writing</a>  and <a href="https://takeielts.britishcouncil.org/sites/default/files/IELTS_Speaking_band_descriptors.pdf" target="_blank" rel="noopener">speaking</a>).</p>
