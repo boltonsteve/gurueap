eef09a2090e7d663fa7b6b652a1111b9df1759c3
Steve Bolton <sgbolton@gmail.com>
2023-08-23 10:43:10 +0800

    new welcome message

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,5 +8,6 @@
         <h2>Academic English not only for Indonesians</h2>
         <p>These posts and pages were originally created as supplementary material for EAP classroom teaching (English for Academic Purposes).</p>
         <p>However, since English often becomes 'lost in translation' from almost any other language, there is likely to be something for everybody.</p>
+        <p>If you're Indonesian and you're preparing for IELTS and/or study abroad, then you've come to the right place!</p>
     </div>
 </div>
