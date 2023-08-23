50fcd5af8229d200389dcaf5a14269e72df1d968
Steve Bolton <sgbolton@gmail.com>
2023-08-23 13:13:49 +0800

    added lemon sqeezy

--- src/lib/songs.js
+++ src/lib/songs.js
@@ -1,8 +1,20 @@
 export const songs = [
     {
+        title: "Lemon Squeezy",
+        href: "lemon-squeezy",
+        keywords: ['song','collocation','vocabulary'],
+        description: "Collocation when using the words 'easy' and 'difficult'."
+    },
+    {
         title: "The Impact Song",
         href: "impact-song",
         keywords: ['song','collocation','vocabulary'],
         description: "Collocation when using the word 'impact'."
+    },
+    {
+        title: "Scissor!",
+        href: "scissor",
+        keywords: ['song','pronunciation','linking'],
+        description: "An exercise in 'juncture' - one of 4 strategies used by native speakers to join words together when speaking."
     }
 ];
--- src/routes/songs/impact-song/+page.svelte
+++ src/routes/songs/impact-song/+page.svelte
@@ -6,6 +6,8 @@
 
     <Youtube id="RjM0_MvwhUE" />
 
+    <h1>The Impact Song</h1>
+
     <p>I've covered this collocation problem in a previous post. Students' first language drives them to produce 'bring an impact to/for'.</p>
 
     <p>I thought that by writing a song featuring the correct collocation, they might be brainwashed into getting it right next time.</p>
