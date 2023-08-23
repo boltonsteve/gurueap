9c8db085964896947f0020bc98fdfd140224335d
Steve Bolton <sgbolton@gmail.com>
2023-08-23 12:35:02 +0800

    added would

---
+++ src/routes/posts/conjunctions/+page.svelte
@@ -0,0 +1,70 @@
+<div class="mb-3 mw-500">
+
+    <h1>Can/Could Will/Would Possibility/Abiltiy</h1>
+
+    <p>I enjoy using Facebook because I could see photos of my friends there.</p>
+
+    <p>There is often confusion about <b><i>can/could</i></b>, <b><i>will/would</i></b>. Sometimes students learn in school that <b><i>could</i></b> and <b><i>would</i></b> are more formal, or more polite than <b><i>can</i></b> and <b><i>will</i></b>.<p>
+    <p class="small">Note: This may be true when you are requesting something, but in IELTS speaking and writing you're usually using <b><i>can</i></b> and <b><i>will</i></b> to communicate possibility or ability rather than to make a request.</p>
+
+<h3>Possibility</h3>
+
+<h5>How possible is it?</h5>
+
+<p class="ml-3 font-italic">I enjoy using Facebook because I <span class="font-weight-bold text-success">can</span> see photos of my friends there.</p>
+
+In this case there is a strong possibility (almost 100%) that I will see my friend's photos on Facebook, and so it is more appropriate to use <b><i>can</i></b>.
+
+<p>Let's imagine a similar situation where there is no possibility:</p>
+
+<p class="ml-3 font-italic">If I <span class="font-weight-bold text-success">had</span> an Internet connection, I <span class="font-weight-bold text-success">could</span> see photos of my friends on Facebook.</p>
+
+<p>Here the writer clearly does not have an Internet connection and so there is no possibility of him seeing his friend's photos. Notice that in this example <span class="font-weight-bold text-success">could</span> is part of a structure called 'second conditional':</p>
+
+<p class="font-weight-bold">If + subj + V2 + ',' + subj + could/would + V1</p>
+
+<p>In the 'second conditional' the situation you are describing is unlikely:</p>
+
+<p class="ml-3 font-italic">If I <span class="font-weight-bold text-success">found</span> a million dollars in the street I <span class="font-weight-bold text-success">would</span> buy a new house.</p>
+
+<p>Second conditional - an unlikely situation - is by far the most common context for <span class="font-weight-bold text-success">could</span> and <span class="font-weight-bold text-success">would</span>.</p>
+
+<h3>Will</h3>
+
+<p>Indonesian students tend to overuse <b><i>will</i></b> because they want to translate <b><i>akan</i></b>. But <b><i>will</i></b> is not used in English as much as <b><i>akan</i></b> is used in Indonesian. Actually there are generally only three situations where <b><i>will</i></b> is suitable:</p>
+
+<p>First conditional - a situation that is highly possible:</p>
+
+<p>Look at those clouds. I forgot my umbrella. If it rains I will get wet!</p>
+
+<p>Predictions:</p>
+
+<p>Look at those dark clouds! It will probably rain soon.</p>
+
+<p>Habits (usually annoying habits)</p>
+
+<p>He drives me crazy. He'll (he will) trim his nails and then leave the cuttings all over the floor for me to clean up!</p>
+
+<h3>Ability</h3>
+
+<p>How able are you (or were you)?</p>
+
+<p>It's unusual to talk about past abilities, because once you acquire an ability, for example the ability to swim, you rarely lose that ability. It would be ridiculous, for example, to write:</p>
+
+<p class="ml-3 font-italic">When I was younger I <span class="font-weight-bold text-success">could</span> swim, but then I forgot how to swim so now I can't.</p>
+
+<p>We generally only lose this kind of ability when something terrible happens to us:</p>
+
+<p>When I was younger I could swim, but then I lost my arms and legs and so now I can't swim.</p>
+
+<p>On the other hand ability can sometimes be a matter of degree. for example we can talk about partial ability, and about changes in our level of ability:</p>
+
+<p>When I was younger I could swim 20km, but now that I'm old I can only swim 20 metres!</p>
+
+<p>Most of the time when we talk about ability we use can - present tense - because, most of the time, we're making a claim that is true now.</p>
+
+<h3>Summing up</h3>
+
+<p>Next time you write <b><i>could</i></b> or <b><i>would</i></b> stop and think. You probably should be writing <b><i>can</i></b> or <b><i>will</i></b>!</p>
+
+</div>
