1709292e2953cfcb6eaf0cd85542436d9a6be9e8
Steve Bolton <sgbolton@gmail.com>
2023-08-25 14:46:31 +0800

    songs added back in

---
+++ src/routes/songs/scissor/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+    import Youtube from "svelte-youtube-embed";
+</script>
+
+<div class="mb-3 mw-500">
+
+    <Youtube id="3wOu3pQqGJw" />
+
+    <h1>Scissor!</h1>
+
+    <p>This song features EAP students preparing for postgraduate study in Australia.</p>
+
+    <p>The idea is to force connected speech so that..</p>
+
+    <p class="ml-3 font-italic">Julia Robert is an actress.</p>
+
+    <p>..becomes:</p>
+
+    <p class="ml-3 font-italic">Julia Robert si sa nactress.</p>
+
+    <p>Can you see how this gives the song its title?</p>
+
+</div>
