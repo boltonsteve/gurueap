98c3ab24be57398d46919637aef2953593052ec7
Steve Bolton <sgbolton@gmail.com>
2023-08-25 15:02:03 +0800

    yet yet yet more stuff

---
+++ src/routes/posts/would/+page.svelte
@@ -0,0 +1,93 @@
+<script>
+    document.title = "Would";
+</script>
+<div class="mb-3 mw-500">
+
+    <h1>Can/Could Will/Would Possibility/Abiltiy</h1>
+
+    <p class="ml-3 font-italic">I enjoy using Facebook because I <span class="font-weight-bold text-danger">could</span> see photos of my friends there.</p>
+
+    <p>There is often confusion about <b><i>can/could</i></b>, <b><i>will/would</i></b>. Sometimes students learn in school that <b><i>could</i></b> and <b><i>would</i></b> are more formal or more polite than <b><i>can</i></b> and <b><i>will</i></b>.<p>
+    <p class="small">Note: This may be true when you are requesting something, but in IELTS speaking and writing you're usually using <b><i>can</i></b> and <b><i>will</i></b> to communicate possibility or ability rather than to make a request.</p>
+
+<h3>Possibility</h3>
+
+<p class="ml-3 font-italic">I enjoy using Facebook because I <span class="font-weight-bold text-success">can</span> see photos of my friends there.</p>
+
+<p>In this case there is a strong possibility (almost 100%) that I will see my friend's photos on Facebook, and so it is more appropriate to use <b><i>can</i></b>.</p>
+
+<p>Let's imagine a similar situation where there is no possibility:</p>
+
+<p class="ml-3 font-italic">If I <span class="font-weight-bold text-success">had</span> an Internet connection, I <span class="font-weight-bold text-success">could</span> see photos of my friends on Facebook.</p>
+
+<p>Here the writer clearly does not have an Internet connection and so there is no possibility of him seeing his friend's photos on Facebook. Notice that in this example <span class="font-weight-bold text-success">could</span> is part of a structure called <i>second conditional</i>:</p>
+
+<p class="font-weight-bold">If + subj + V2 + ',' + subj + could/would + V1</p>
+
+<p>In the <i>second conditional</i> the situation you are describing is <span class="uli">unlikely</span>:</p>
+
+<p class="ml-3 font-italic">If I <span class="font-weight-bold text-success">found</span> a million dollars in the street I <span class="font-weight-bold text-success">would</span> buy a new house.</p>
+
+<p><i>Second conditional</i> - an unlikely situation - is by far the most common context for <span class="font-weight-bold text-success">could</span> and <span class="font-weight-bold text-success">would</span>.</p>
+
+<h3>Will</h3>
+
+<p>Indonesian students tend to overuse <b><i>will</i></b> because they want to translate <b><i>akan</i></b>. But <b><i>will</i></b> is not used in English as much as <b><i>akan</i></b> is used in Indonesian. Actually there are generally only three situations where <b><i>will</i></b> is suitable:</p>
+
+<p><b>First conditional - a situation that is highly possible:</b></p>
+
+<ul class="font-italic">
+    <li>
+        Look at those clouds. I forgot my umbrella. If it rains I <span class="font-weight-bold text-success">will</span> get wet!
+    </li>
+</ul>
+
+<p><b>Predictions:</b></p>
+
+<ul class="font-italic">
+    <li>
+        Look at those dark clouds! It <span class="font-weight-bold text-success">will</span> probably rain soon.
+    </li>
+</ul>
+
+<p><b>Habits (often annoying habits), and Routines:</b></p>
+
+<p class="ml-3">Present habits/routines</p>
+
+<ul class="font-italic">
+    <li>He drives me crazy. He'll (= he will) trim his nails and then leave the cuttings all over the floor for me to clean up!</li>
+    <li class="lst-none small mb-1">(An annoying habit..)</li>
+    <li>On a normal work day I'll arrive at work at around 8 and go straight to the canteen for a coffee.</li>
+    <li class="lst-none small">(A daily routine. Note, however, that native speakers tend to talk about present routines using present simple tense without 'will'!)</li>
+</ul>
+
+<p class="ml-3">Past habits/routines</p>
+
+<ul class="font-italic">
+    <li>My grandfather was extremely kind. He'd (= he would) always go out of his way to help people.</li>
+    <li class="lst-none small mb-1">(A past habit, this time a good habit!)</li>
+    <li>When I was a student I would often go to bed late because I had to finish assignments.</li>
+    <li class="lst-none small">(A past routine - I'm not a student any more. This is a bit like saying 'used to')</li>
+</ul>
+
+<h3>Ability</h3>
+
+<p>It's unusual to talk about past abilities, because once you acquire an ability, for example the ability to swim, you rarely lose that ability. It would be ridiculous, for example, to write:</p>
+
+<p class="ml-3 font-italic">When I was younger I could swim, but then I forgot how to swim so now I can't.</p>
+
+<p>We generally only lose this kind of ability when something terrible happens to us:</p>
+
+<p class="ml-3 font-italic">When I was younger I <span class="font-weight-bold text-success">could</span> swim, but then I lost my arms and legs in an accident and so now I can't swim.</p>
+
+<p>On the other hand ability can sometimes be a matter of degree. For example we can talk about partial ability, and about changes in our level of ability:</p>
+
+<p class="ml-3 font-italic">When I was younger I <span class="font-weight-bold text-success">could</span> swim 20km, but now that I'm old I <span class="font-weight-bold text-success">can</span> only swim 20 metres!</p>
+
+<p>Most of the time when we talk about ability we use <b><i>can</i></b> - present tense - because, most of the time, we're making a claim that is true now.</p>
+
+<h3>Summing up</h3>
+
+<p>Next time you speak or write <b><i>could</i></b> or <b><i>would</i></b> stop and think. You probably should be writing <b><i>can</i></b> or <b><i>will</i></b>!</p>
+
+</div>
