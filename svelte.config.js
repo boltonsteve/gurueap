898a297db892f241d4ef74376250d57d4571aabc
Steve Bolton <sgbolton@gmail.com>
2023-08-25 23:16:11 +0800

    change adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,26 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
+ 
+export default {
+  kit: {
+    adapter: adapter({
+      // see the 'Deployment configuration' section below
+    })
+  }
+};
+
+// import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
+// const config = {
+	// kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
-};
+		// adapter: adapter()
+	// }
+// };
 
-export default config;
+// export default config;
 
 
 // import adapter from '@sveltejs/adapter-static';
