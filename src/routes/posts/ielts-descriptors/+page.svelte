aae7a825cca5a3e19374e02d6d41cdca5cdfc700
Steve Bolton <sgbolton@gmail.com>
2023-08-27 10:59:35 +0800

    band descriptors update

--- src/routes/posts/ielts-descriptors/+page.svelte
+++ src/routes/posts/ielts-descriptors/+page.svelte
@@ -1,19 +1,50 @@
 <script>
+
     import { slide } from 'svelte/transition';
     import {descriptors} from '$lib/descriptors.js';
 
-    let speaking = true;
-    let task1 = false;
-    let task2 = false;
+    let skillCodes = [
+        'speaking',
+        'task1',
+        'task2'
+    ];
 
-    let headings = ['speaking','task1','task2'];
-    let headingTexts = ['Speaking','Task 1 Writing','Task 2 Writing'];
+    let headingTexts = [
+        'Speaking',
+        'Task 1 Writing',
+        'Task 2 Writing'
+    ];
+
+    let sHead = [
+        '&nbsp;',
+        'Fluency and Coherence',
+        'Lexical Resource',
+        'Grammatical Range and Accuracy',
+        'Pronunciation'
+    ];
+
+    let tHead = [
+        '&nbsp;',
+        'Task Achievement',
+        'Coherence and Cohesion',
+        'Lexical Resource',
+        'Grammatical Range and Accuracy'
+    ];
+
+    let ttHead = [
+        '&nbsp;',
+        'Task Response',
+        'Coherence and Cohesion',
+        'Lexical Resource',
+        'Grammatical Range and Accuracy'
+    ];
+
+    let headers = [sHead, tHead, ttHead];
 
     let skills = descriptors.map( next => {
         let criteria = next.map( next => {
             let cols = next.map( next => {
                 let bulls = next.map( next => next )
-                console.log(bulls);
                 return `<p class="mt-0">${bulls.join('</p><p>')}</p>`;
             })
             return cols;
@@ -22,33 +53,28 @@
     })
 
     const getDescriptors = (skill) => {
-
-        let sk = headings.indexOf(skill);
-        console.log("sk: " + sk);
-
-        let sRows = [];
+        let skillId = skillCodes.indexOf(skill);
+        let head = headers[skillId].map( next => `<h3 class="mb-0 px-1">${next}</h3>` );
+        let rows = [];
+        rows.push(head.join(''));
         for(let i=9;i>=0;i--) {
-            let row = [`<div class="text-center pl-1" style="font-size:2rem;">${i}</div>`];
+            let row = [`<div class="text-center pl-1 " style="font-size:2rem;">${i}</div>`];
             for(let n=0;n<4;n++) {
-                let next = skills[sk][n][i];
-                row.push(`<div>${next}</div>`);
+                let next = skills[skillId][n][i];
+                row.push(`<div class="px-1" style="border-bottom:1px solid black;">${next}</div>`);
             }
-            sRows.push(row.join(''));
+            rows.push(row.join(''));
             row = [];
         }
-
         return `
-            <h1 class="mt-1 text-center">${headingTexts[sk]}</h1>
-            <div class="flex row-col cols-14444 mb-3">${sRows.join('')}</div>
+            <h1 class="mt-1 text-center">${headingTexts[skillId]}</h1>
+            <div class="flex row-col cols-14444 mb-3" style="grid-column-gap:0px;>${rows.join('')}</div>
         `;
-
     }
 
     let sHtml = getDescriptors('speaking');
     let tHtml = getDescriptors('task1');
     let ttHtml = getDescriptors('task2');
-
-    console.log(sHtml);
 
     function showS() {
         speaking = true;
@@ -68,6 +94,10 @@
         task2 = true;
     }
 
+    let speaking = true;
+    let task1 = false;
+    let task2 = false;
+
 </script>
 
 <svelte:head>
@@ -84,20 +114,24 @@
     </div>
 
 {#if speaking}
-    <div transition:slide={{duration:1000}}>
+    <div in:slide={{duration:1000}} out:slide={{duration:1000}}>
         {@html sHtml}
     </div>
 {/if}
 {#if task1}
-    <div transition:slide={{duration:1000}}>
+    <div in:slide={{duration:1000}} out:slide={{duration:1000}}>
     {@html tHtml}
     </div>
 {/if}
 {#if task2}
-    <div transition:slide={{duration:1000}}>
+    <div in:slide={{duration:1000}} out:slide={{duration:1000}}>
     {@html ttHtml}
     </div>
 {/if}
 
 
 </div>
+
+<style>
+
+</style>
