a3665175e9b38e383920efe615614f0775ca7686
Steve Bolton <sgbolton@gmail.com>
2023-09-02 15:57:10 +0800

    seo stuff 22

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,7 +15,7 @@
     <div>
         <img src="/img/gurueap.jpg" class="img-fluid rounded" alt="GuruEAP">
         <h2>Academic English not only for Indonesians</h2>
-        <p>GuruEAP was originally conceived as supplementary material for EAP classroom teaching (English for Academic Purposes) in Indonesia. Posts deal with various academic and linguistic challenges facing all international students preparing for IELTS and for study abroad.</p>
+        <p>GuruEAP was originally conceived as supplementary material for EAP classroom teaching (English for Academic Purposes) in Indonesia. Posts deal with the various academic and linguistic challenges faced by all international students preparing for IELTS and for both graduate and postgraduate study abroad.</p>
         <p>{@html flagIndo} I like to think there's something for everybody here, but certainly if you're Indonesian and you're preparing for IELTS and/or study abroad, then you've come to the right place!</p>
     </div>
 </div>
