07b2b79095767e5125e4c59f89422061cba96060
Steve Bolton <sgbolton@gmail.com>
2023-09-06 23:05:26 +0800

    responsive

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,6 +14,7 @@
         <h2>Academic English not only for Indonesians</h2>
         <p>GuruEAP was originally conceived as supplementary material for EAP classroom teaching (English for Academic Purposes) in Indonesia. Posts deal with the various academic and linguistic challenges faced by all international students preparing for IELTS and for both graduate and postgraduate study abroad.</p>
         <p>I like to think there's something for everybody here, but certainly if you're {@html flagIndo}Indonesian{@html flagIndo} and you're preparing for IELTS and/or study in an English-speaking university, then you've come to the right place!</p>
+        <img src="/img/responsive.png" alt="responsive design" style="width:220px;float:right;">
         <p>GuruEAP uses a 'mobile first' design, making content easy to access on any device.</p>
         <p>If there are any special requests for additional content, or any questions about what you see here, please contact <a href="mailto:gurueap@gmail.com">gurueap@gmail.com</a>.</p>
     </div>
