b26b3dd2fe54c3cd103cedd30bc411a7c8455686
Steve Bolton <sgbolton@gmail.com>
2023-10-18 11:32:26 +0800

    those 2

--- src/routes/posts/those/+page.svelte
+++ src/routes/posts/those/+page.svelte
@@ -8,7 +8,7 @@
     
     <img src="/img/those.jpg" class="img-fluid" alt="those">
 
-    <div class="alert alert-danger font-italic">Many people in the region cannot afford to send their children to school. <span class="font-weight-bold text-danger">Those</span> people receive some financial assistance from the government, but it is not enough.</div>
+        <div class="alert alert-danger font-italic">Teenagers these days can't wait to buy the latest handphones and the latest laptops. <span class="font-weight-bold text-danger">Those</span> consumer products are generally expensive.</div>
 
     <p>Ok let's start with the example given in the picture above. Although they are not mentioned separately, we can see that the writer is implicitly referring to two groups of people:</p>
 
@@ -21,40 +21,42 @@
 
     <div class="alert alert-success font-italic">Some people believe in magic. Other people do not believe in magic. <span class="font-weight-bold text-success">Those</span> who do not believe in magic will never find it.</div>
 
-    <p>The writer is using <b><i>those</i></b> to focus on one of the groups.</p>
+    <p>The writer is using <b><i>those</i></b> to focus on <span class="uli">one of the groups</span>.</p>
 
     <p>This is the function of <b><i>those</i></b>.</p>
 
-    <p>If we return to the financial assistance example, the focus is on only one group - <i>people who cannot afford to send their children to school</i>. In which case you need <b><i>these</i></b>:</p>
+    <hr>
 
-    <div class="alert alert-success font-italic">Many people in the region cannot afford to send their children to school. <span class="font-weight-bold text-success">These</span> people receive some financial assistance from the government, but it is not enough.</div>
+    <p>If we return to the handphones and laptops example, the focus is on only one group of things - <i>consumer products</i>. In which case you need <b><i>these</i></b>:</p>
 
-    <p>Whenever you are dealing with more than one group of things or people and you want to focus on one of the groups, use <span class="font-weight-bold text-success">those</span>. Here is another example:</p>
+        <div class="alert alert-success font-italic">Teenagers these days can't wait to buy the latest handphones and the latest laptops. <span class="font-weight-bold text-success">These</span> consumer products are generally expensive.</div>
+
+    <p>Only use <b><i>those</i></b> when you are dealing with more than one group of things or people and <span class="uli">you want to focus on one of the groups</span>. Here is another example:</p>
 
     <div class="alert alert-success font-italic">These days smartphones are produced by a wide range of manufacturers. <span class="font-weight-bold text-success">Those</span> made by Apple are beautifully designed, but tend to be rather expensive.
         <p class="small mb-0">(the writer also has in mind Samsung, Nokia, etc.)</p>
     </div>
 
-    <p>Very often those is followed immediately by a relative pronoun - who, that, which, etc.:</p>
+    <hr>
 
-    <ul class="lh-15">
-        <li>It makes <b><i>those who</i></b> appoint judges understandably cautious.</li>
-        <li><b><i>Those who</i></b> regularly drink milk feel less stressed.</li>
-        <li>The greatest ideas are <b><i>those that</i></b> are the simplest.</li>
-        <li>Many of <b><i>those that</i></b> remain are very small.</li>
-        <li>The only houses with weaker protections are <b><i>those which</i></b> remain empty.</li>
-        <li>A special category of ECUs are <b><i>those which</i></b> are programmable.</li>
-        <li>The very best dinner parties are <b><i>those where</i></b> you meet new best friends.</li>
-        <li>The most boring presentations are <b><i>those where</i></b> the speaker reads the words on the screen.</li>
+    <p>Very often <span class="font-weight-bold text-success font-italic">those</span> is followed immediately by a <span class="font-weight-bold text-primary font-italic">relative pronoun</span> - <b><i>who</i></b>, <b><i>that</i></b>, <b><i>which</i></b>, etc.:</p>
+    
+    <ul class="lh-15 font-italic">
+        <li>It makes <span class="font-weight-bold text-success">those</span> <span class="font-weight-bold text-primary">who</span> appoint judges understandably cautious.</li>
+        <li><span class="font-weight-bold text-success">Those</span> <span class="font-weight-bold text-primary">who</span> regularly drink milk feel less stressed.</li>
+        <li>The greatest ideas are <span class="font-weight-bold text-success">those</span> <span class="font-weight-bold text-primary">that</span> are the simplest.</li>
+        <li>Many of <span class="font-weight-bold text-success">those</span> <span class="font-weight-bold text-primary">that</span> remain are very small.</li>
+        <li>The only houses with weaker protections are <span class="font-weight-bold text-success">those</span> <span class="font-weight-bold text-primary">which</span> remain empty.</li>
+        <li>A special category of ECUs are <span class="font-weight-bold text-success">those</span> <span class="font-weight-bold text-primary">which</span> are programmable.</li>
     </ul>
 
-    <p>Notice that in all of the preceding examples, the relative pronoun is followed by a dependent clause (e.g. 'who appoint judges understandably cautious').</p>
+    <p>Notice that <span class="font-weight-bold text-success font-italic">those</span> <span class="font-weight-bold text-primary font-italic">where</span> and <span class="font-weight-bold text-success">those</span> <span class="font-weight-bold text-primary font-italic">in which</span> are followed by an <span class="font-weight-bold text-info font-italic">independent clause</span>:</p>
 
-    <p>Be careful with <b><i>those in which</i></b>. This is followed by an independent clause.</p>
-
-    <ul class="lh-15">
-        <li>The stories that people most love are <b><i>those in which</i></b> characters overcome unusual limitations.</li>
-        <li>Matrifocal societies are <b><i>those in which</i></b> women, especially mothers, occupy a central position.</li>
+    <ul class="lh-15 font-italic">
+        <li>The very best dinner parties are <span class="font-weight-bold text-success">those</span> <span class="font-weight-bold text-primary">where</span> you meet new best friends.</li>
+        <li>The most boring presentations are <span class="font-weight-bold text-success">those</span> <span class="font-weight-bold text-primary">where</span> the speaker reads the words on the screen.</li>
+        <li>The stories that people most love are <span class="font-weight-bold text-success">those</span> <span class="font-weight-bold text-primary">in which</span> <span class="font-weight-bold text-info">characters overcome unusual limitations</span>.</li>
+        <li>Matrifocal societies are <span class="font-weight-bold text-success">those</span> <span class="font-weight-bold text-primary">in which</span> <span class="font-weight-bold text-info">women, especially mothers, occupy a central position</span>.</li>
     </ul>
 
 </div>
