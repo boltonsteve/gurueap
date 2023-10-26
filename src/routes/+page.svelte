87ff48f372d04f01b6b7c830fc80ca9942c471ab
Steve Bolton <sgbolton@gmail.com>
2023-10-26 22:32:42 +0800

    landing 3

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -22,36 +22,34 @@
 <div id="landing_grid">
 
     <div id="div_top">
-        <div id="title" class="mb-0">GuruEAP</div>
-        <div id="tag" class="mt-0">Academic English not only for Indonesians</div>
+        <div id="title">GuruEAP</div>
+        <div id="tag">Academic English not only for Indonesians</div>
+    </div>
+
+    <div id="div_bottom" class="my-3">
+        <div class="card">
+            <a href="./posts">
+                <div><FilePost height={hw} width={hw} /></div>
+                Posts
+            </a>
+        </div>
+        <div class="card">
+            <a href="./games">
+                <div><Controller  height={hw} width={hw} /></div>
+                Games
+            </a>
+        </div>
+        <div class="card">
+            <a href="./songs">
+                <div><FileMusic  height={hw} width={hw} /></div>
+                Songs
+            </a>
+        </div>
     </div>
 
     <div id="div_middle">
-
         <div>GuruEAP was originally conceived as supplementary material for EAP classroom teaching (English for Academic Purposes) in Indonesia.</div>
         <div>There's something for everybody, but certainly if you're Indonesian and you're preparing for IELTS and/or study in an English-speaking university, then you've come to the right place!</div>
-
-    </div>
-
-    <div id="div_bottom">
-        <a href="./posts">
-            <div class="card">
-                <div><FilePost height={hw} width={hw} /></div>
-                Posts
-            </div>
-        </a>
-        <a href="./games">
-            <div class="card">
-                <div><Controller  height={hw} width={hw} /></div>
-                Games
-            </div>
-        </a>
-        <a href="./songs">
-            <div class="card">
-                <div><FileMusic  height={hw} width={hw} /></div>
-                Songs
-            </div>
-        </a>
     </div>
 
 </div>
@@ -67,7 +65,7 @@
     }
 
     #tag {
-        font-size:2rem;
+        font-size:1.6rem;
         text-align:center;
         font-style:italic;
     }
@@ -78,10 +76,8 @@
         flex-wrap: wrap;
         justify-content: center;
         align-items: center;
-        height: 80vh;
         column-gap: 20px;
-        max-width: 800px;
-        max-height: 400px;
+        max-width: 700px;
         margin: 30px auto;
     }
 
@@ -95,32 +91,35 @@
         gap: 10px;
     }
 
+    #div_middle div {
+        max-width:300px;
+    }
+
     #div_middle {
         display: grid;
         grid-template-columns: 1fr 1fr;
         gap: 20px;
-        padding: 20px;
+        padding: 0px 20px 0px 20px;
     }
 
     .card {
         display:grid;
         grid-template-columns: 1fr;
         text-align:center;
-        justify-content:center;
+        justify-content:space-around;
         align-items:center;
-        width:200px;
-        padding:20px;
-        border:1px solid #bbbbbb;
-        border-radius:10px;
         font-size:1.2rem;
     }
 
     #div_bottom {
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: center;
+        display:grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        width:100%;
+        /* max-width:500px; */
+        /* display: flex; */
+        justify-content: space-around;
         align-items: center;
-        gap: 20px;
+        /* gap: 20px; */
     }
 
     a {
