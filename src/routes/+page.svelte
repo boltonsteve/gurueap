5cfc4acd2ca2955987aba9cc7f02bd8afcef4773
Steve Bolton <sgbolton@gmail.com>
2023-10-27 20:41:40 +0800

    landing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,10 @@
 <script>
     import { MusicNoteBeamed, FilePost, FileMusic, Globe, Controller } from "svelte-bootstrap-icons";
     import SvelteSeo from "svelte-seo"
-    import { flagIndo } from '$lib/flags.js'
-    import Card from './components/Card.svelte'
-    import { students } from '$lib/mugshots.js';
     import { links } from '$lib/links.js';
 
     let totalLinks = links.length;
     let hw = 50;
-
-    students.sort(() => Math.random() - 0.5);
 
 </script>
 
@@ -19,14 +14,20 @@
     keywords="academic english,english for academic purposes,EAP,IELTS,international students,pre-departure,Indonesia"
 />
 
+<div id="wrapper">
 <div id="landing_grid" class="mx-auto">
 
     <div id="div_top">
-        <div id="title" class="my-0 py-0">GuruEAP</div>
-        <div id="tag" class="my-0 py-0">Academic English not only for Indonesians</div>
+        <div id="title">GuruEAP</div>
+        <div id="tag">Academic English not only for Indonesians</div>
     </div>
 
-    <div id="div_bottom" class="my-3">
+    <div id="div_middle">
+        <div class="text-box">GuruEAP was originally conceived as supplementary material for EAP classroom teaching (English for Academic Purposes) in Indonesia.</div>
+        <div class="text-box">There's something for everybody, but certainly if you're Indonesian and you're preparing for IELTS and/or study in an English-speaking university, then you've come to the right place!</div>
+    </div>
+
+    <div id="div_bottom">
         <div class="card">
             <a href="./posts">
                 <div><FilePost height={hw} width={hw} /></div>
@@ -47,28 +48,33 @@
         </div>
     </div>
 
-    <div id="div_middle">
-        <div>GuruEAP was originally conceived as supplementary material for EAP classroom teaching (English for Academic Purposes) in Indonesia.</div>
-        <div>There's something for everybody, but certainly if you're Indonesian and you're preparing for IELTS and/or study in an English-speaking university, then you've come to the right place!</div>
-    </div>
-
 </div>
 
+</div>
 
 <style>
 
     /* * { */
-    /*     border: 1px solid green; */
+    /*     border: 1px solid red; */
     /* } */
+
+    #wrapper {
+        height:80vh;
+        display: grid;
+        max-width:700px
+    }
+
+    #landing_grid {
+        display: grid;
+        grid-template-columns: 1fr;
+        gap: 30px;
+    }
 
     #div_top {
         display: grid;
         grid-template-columns: 1fr;
         justify-content: center;
         align-items: center;
-        text-align:center;
-        gap: 10px;
-        /* grid-auto-flow: row; */
     }
 
     #title {
@@ -76,62 +82,41 @@
         text-align:center;
         font-weight:bold;
         color: #aaaaaa;
-        margin-bottom:0px;
-        padding-bottom:0px;
     }
 
     #tag {
-        font-size:0.9rem;
+        font-size:1.4rem;
         text-align:center;
         font-style:italic;
-    }
-
-    #landing_grid {
-        display: grid;
-        grid-template-columns: 1fr;
-        /* display: flex; */
-        /* flex-wrap: wrap; */
-        justify-content: center;
-        align-items: center;
-        column-gap: 20px;
-        max-width: 600px;
-        margin: 30px auto;
     }
 
     #div_middle {
         display: grid;
         grid-template-columns: 1fr 1fr;
-        justify-content: center;
         gap: 20px;
     }
 
     .card {
         display:grid;
-        grid-template-columns: 1fr;
-        text-align:center;
-        justify-content:space-around;
-        align-items:center;
+        justify-content:center;
         font-size:1.2rem;
     }
 
     #div_bottom {
-        display:grid;
-        grid-template-columns: 1fr 1fr 1fr;
-        /* display: flex; */
+        display: flex;
         justify-content: space-around;
-        align-items: center;
-        /* gap: 20px; */
     }
 
     a {
         text-decoration:none;
+        text-align:center;
     }
 
-    @media(max-width:650px) {
-        #div_middle {
-            display: grid;
-            grid-template-columns: 1fr;
-        }
-    }
+    /* @media(max-width:650px) { */
+    /*     #div_middle { */
+    /*         display: grid; */
+    /*         grid-template-columns: 1fr; */
+    /*     } */
+    /* } */
 
 </style>
