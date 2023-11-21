8715d9c9e9b15a5071dcc0ee9a31165db9e73b99
Steve Bolton <sgbolton@gmail.com>
2023-11-21 12:27:40 +0800

    which 2

--- src/routes/t/art-of-which/+page.svelte
+++ src/routes/t/art-of-which/+page.svelte
@@ -1,7 +1,6 @@
 <script>
 
-    import { PeopleFill } from "svelte-bootstrap-icons"
-    import { scale, fade } from 'svelte/transition'
+    import Multiplayer from '../../components/Multiplayer.svelte'
 
     let qa = [
         {
@@ -46,144 +45,8 @@
         }
     ]
 
-    let peopleSize = 50;
-
-    let title = "the art of 'which'"
-    let showQuestion = false;
-    let showAnswer = false;
-
-    let qna = {};
-    let question = ''
-    let answer = ''
-    let players = 2
-
-    let men = [
-        {
-            id: 0,
-            color:'green',
-            score: 0
-        },
-        {
-            id: 1,
-            color:'blue',
-            score: 0
-        },
-        {
-            id: 2,
-            color:'red',
-            score: 0
-        },
-        {
-            id: 3,
-            color:'orange',
-            score: 0
-        }
-    ]
-   
-    let onKeyUp = (e) => {
-        if (e.key == 'g') {
-            men[0].score ++
-        } else if (e.key == 'G') {
-            men[0].score --
-        } else if (e.key == 'b') {
-            men[1].score ++
-        } else if (e.key == 'B') {
-            men[1].score --
-        } else if (e.key == 'r') {
-            men[2].score ++
-        } else if (e.key == 'R') {
-            men[2].score --
-        } else if (e.key == 'o') {
-            men[3].score ++
-        } else if (e.key == 'O') {
-            men[3].score --
-        } else if (e.key == 'a') {
-            showAnswer = true;
-        } else if (e.key == 'n') {
-            doNext();
-        }
-    }
-
-    function doNext() {
-        showQuestion = false;
-        showAnswer = false;
-        qna = qa.shift();
-        question = qna.q;
-        answer = qna.a;
-        showQuestion = true;
-    }
+    let players = 3
 
 </script>
 
-
-<div id="container">
-
-    <div id="header">
-
-        {#each {length: players} as _, i}
-            <div id="{men[i].id}">
-                <PeopleFill fill="{men[i].color}" width={peopleSize} height={peopleSize} />
-                <div>{men[i].score}</div>
-            </div>
-        {/each}
-
-    </div>
-
-    <div id="title">{title}</div>
-
-    <div id="question">
-        {#if showQuestion}
-            <div transition:scale>{question}</div>
-        {/if}
-    </div>
-
-    <div id="answer">
-        {#if showAnswer}
-            <div transition:scale>{answer}</div>
-        {/if}
-    </div>
-
-</div>
-
-<svelte:window on:keyup|preventDefault={onKeyUp} />
-
-<style>
-
-/* div { */
-/*     border:1px solid red */
-/* } */
-
-#container {
-    height:100vh;
-    display:grid;
-    grid-auto-flow: column;
-    grid-template-rows: 90px 40px 1fr 1fr;
-}
-
-#header {
-    display:flex;
-    justify-content:space-around;
-}
-
-#header > div {
-    text-align:center;
-    font-size:1.5rem;
-}
-
-#title {
-    display:flex;
-    justify-content:center;
-    align-items:center;
-    font-size: 2rem;
-    color: var(--light);
-    background: var(--grey);
-}
-
-#question, #answer {
-    font-size:1.5rem;
-    display:flex;
-    justify-content:center;
-    align-items:center;
-}
-
-</style>
+<Multiplayer qa={qa} players={players} />
