2ba677c9130832ed37739c3e6c253a66a3c72817
stevebolton <sgbolton@gmail.com>
2024-08-20 07:10:16 +0800

    multiplayer vocab 8

---
+++ src/routes/t/cvfori-8/+page.svelte
@@ -0,0 +1,171 @@
+<script>
+
+import Multiplayer from '../../components/Multiplayer.svelte'
+
+let qa = [
+    {
+        q: [
+            '<div>During the Colonial_Era, Indonesia was occupied by the Dutch.</div>'
+        ],
+        a: [
+            '<div>During the Colonial_Era, Indonesia was occupied by the Dutch.</div>'
+        ]
+    },
+    {
+        q: [
+            '<div>Archeological_evidence suggests the city of Damascus was founded six thousand years ago.</div>'
+        ],
+        a: [
+            '<div>Archeological_evidence suggests the city of Damascus was founded six thousand years ago.</div>'
+        ]
+    },
+    {
+        q: [
+            '<div>The police organised the cases in chronological_order.</div>'
+        ],
+        a: [
+            '<div>The police organised the cases in chronological_order.</div>'
+        ]
+    },
+    {
+        q: [
+            '<div>In the_Middle_Ages, the only large buildings were castles and cathedrals.</div>'
+        ],
+        a: [
+            '<div>In the_Middle_Ages, the only large buildings were castles and cathedrals.</div>'
+        ]
+    },
+    {
+        q: [
+            '<div>In the late_twentieth_century, many communist countries turned to capitalism.</div>'
+        ],
+        a: [
+            '<div>In the late_twentieth_century, many communist countries turned to capitalism.</div>'
+        ]
+    },
+    {
+        q: [
+            '<div>In recent_years, China has become the world’s most dynamic economy.</div>'
+        ],
+        a: [
+            '<div>In recent_years, China has become the world’s most dynamic economy.</div>'
+        ]
+    },
+    {
+        q: [
+            '<div>Prehistoric_remains/ruins, including stone circles and graves, can be found in many parts of the UK.</div>'
+        ],
+        a: [
+            '<div>Prehistoric_remains/ruins, including stone circles and graves, can be found in many parts of the UK.</div>'
+        ]
+    },
+    {
+        q: [
+            '<div>The Olympic Games were first held during classical_times.</div>'
+        ],
+        a: [
+            '<div>The Olympic Games were first held during classical_times.</div>'
+        ]
+    },
+    {
+        q: [
+            '<div>Long_ago, sailors from Sumatra crossed the Indian Ocean and settled in Madagascar.</div>'
+        ],
+        a: [
+            '<div>Long_ago, sailors from Sumatra crossed the Indian Ocean and settled in Madagascar.</div>'
+        ]
+    },
+    {
+        q: [
+            '<div>Despite the comforts of the modern world, people often feel nostalgic for the past.</div>'
+        ],
+        a: [
+            '<div>Despite the comforts of the modern world, people often feel nostalgic for the past.</div>'
+        ]
+    },
+    {
+        q: [
+            '<div>In the post-9/11_years, there has been a greater focus on soft diplomacy.</div>'
+        ],
+        a: [
+            '<div>In the post-9/11_years, there has been a greater focus on soft diplomacy.</div>'
+        ]
+    },
+    {
+        q: [
+            '<div>The early_twentieth_century witnessed the emergence of mass culture, including cinema and popular music.</div>'
+        ],
+        a: [
+            '<div>The early_twentieth_century witnessed the emergence of mass culture, including cinema and popular music.</div>'
+        ]
+    },
+    {
+        q: [
+            '<div>During the Cold_War years, the USSR and the USA confronted each other with the threat of mutually assured destruction.</div>'
+        ],
+        a: [
+            '<div>During the Cold_War years, the USSR and the USA confronted each other with the threat of mutually assured destruction.</div>'
+        ]
+    },
+    {
+        q: [
+            '<div>The UK is often viewed as the birthplace of the Industrial_Revolution.</div>'
+        ],
+        a: [
+            '<div>The UK is often viewed as the birthplace of the Industrial_Revolution.</div>'
+        ]
+    },
+    {
+        q: [
+            '<div>Because of the advent of the Internet, the current era is sometimes known as the Information_Age.</div>'
+        ],
+        a: [
+            '<div>Because of the advent of the Internet, the current era is sometimes known as the Information_Age.</div>'
+        ]
+    },
+    {
+        q: [
+            '<div>The New_Order came to an end when the Asian Financial Crisis struck in 1997-98.</div>'
+        ],
+        a: [
+            '<div>The New_Order came to an end when the Asian Financial Crisis struck in 1997-98.</div>'
+        ]
+    },
+    {
+        q: [
+            '<div>Central Java is full of ancient_monuments, such as Prambanan and Borobudur.</div>'
+        ],
+        a: [
+            '<div>Central Java is full of ancient_monuments, such as Prambanan and Borobudur.</div>'
+        ]
+    },
+    {
+        q: [
+            '<div>The voyages of Columbus, Magellan and Vasco da Gama heralded the Age_of_Discovery.</div>'
+        ],
+        a: [
+            '<div>The voyages of Columbus, Magellan and Vasco da Gama heralded the Age_of_Discovery.</div>'
+        ]
+    },
+    {
+        q: [
+            '<div>Since_independence, Indonesia’s population has multiplied several times.</div>'
+        ],
+        a: [
+            '<div>Since_independence, Indonesia’s population has multiplied several times.</div>'
+        ]
+    },
+    {
+        q: [
+            '<div>Should old buildings be preserved for posterity or should they simply be knocked down?</div>'
+        ],
+        a: [
+            '<div>Should old buildings be preserved for posterity or should they simply be knocked down?</div>'
+        ]
+    }
+]
+
+
+</script>
+
+<Multiplayer title="Time and History" qa={qa} />
---
+++ src/routes/t/cvfori-9/+page.svelte
@@ -0,0 +1,170 @@
+<script>
+
+import Multiplayer from '../../components/Multiplayer.svelte'
+
+let qa = [
+    {
+        q:[
+            '<div>The best-known __________ in Sydney is the Opera House.</div>'
+        ],
+        a:[
+            '<div>The best-known <b><i>tourist attraction</i></b> in Sydney is the Opera House.</div>'
+        ]
+    },
+    {
+        q:[
+            '<div>In the first few days, visitors to India may suffer from __________.</div>'
+        ],
+        a:[
+            '<div>In the first few days, visitors to India may suffer from <b><i>culture shock</i></b>.</div>'
+        ]
+    },
+    {
+        q:[
+            '<div>Montego Bay is a popular __________ in Jamaica.</div>'
+        ],
+        a:[
+            '<div>Montego Bay is a popular <b><i>holiday resort</i></b> in Jamaica.</div>'
+        ]
+    },
+    {
+        q:[
+            '<div>In Europe, the __________ for tourism is July-August.</div>'
+        ],
+        a:[
+            '<div>In Europe, the <b><i>peak season</i></b> for tourism is July-August.</div>'
+        ]
+    },
+    {
+        q:[
+            '<div>Nepal has established itself as a popular destination for __________ such as trekking and mountaineering.</div>'
+        ],
+        a:[
+            '<div>Nepal has established itself as a popular destination for <b><i>adventure tourism</i></b> such as trekking and mountaineering.</div>'
+        ]
+    },
+    {
+        q:[
+            '<div>In the long run, the best solution to traffic congestion is free, convenient __________.</div>'
+        ],
+        a:[
+            '<div>In the long run, the best solution to traffic congestion is free, convenient <b><i>public transport</i></b>.</div>'
+        ]
+    },
+    {
+        q:[
+            '<div>Environmentally-conscious travelers have triggered a growth in __________.</div>'
+        ],
+        a:[
+            '<div>Environmentally-conscious travelers have triggered a growth in <b><i>eco-tourism</i></b>.</div>'
+        ]
+    },
+    {
+        q:[
+            '<div>Sadly, an influx of Western holiday-makers has resulted in __________.</div>'
+        ],
+        a:[
+            '<div>Sadly, an influx of Western holiday-makers has resulted in <b><i>cultural degradation</i></b>.</div>'
+        ]
+    },
+    {
+        q:[
+            '<div>Jim once visited Japan on a __________ but didn’t have time to do any sightseeing.</div>'
+        ],
+        a:[
+            '<div>Jim once visited Japan on a <b><i>business trip</i></b> but didn’t have time to do any sightseeing.</div>'
+        ]
+    },
+    {
+        q:[
+            '<div>Most tourists come to the outback to enjoy the __________.</div>'
+        ],
+        a:[
+            '<div>Most tourists come to the outback to enjoy the <b><i>unspoiled scenery</i></b>.</div>'
+        ]
+    },
+    {
+        q:[
+            '<div>People say that travel __________ the __________, but it is not always true.</div>'
+        ],
+        a:[
+            '<div>People say that travel <b><i>broadens</i></b> the <b><i>mind</i></b>, but it is not always true.</div>'
+        ]
+    },
+    {
+        q:[
+            '<div>With all these __________ airlines, international travel has become more affordable.</div>'
+        ],
+        a:[
+            '<div>With all these <b><i>low-cost</i></b> airlines, international travel has become more affordable.</div>'
+        ]
+    },
+    {
+        q:[
+            '<div>In reality, the __________ do not always benefit from tourism.</div>'
+        ],
+        a:[
+            '<div>In reality, the <b><i>local inhabitants</i></b> do not always benefit from tourism.</div>'
+        ]
+    },
+    {
+        q:[
+            '<div>You may suffer from __________ if you fly from Europe to the Far East.</div>'
+        ],
+        a:[
+            '<div>You may suffer from <b><i>jet-lag</i></b> if you fly from Europe to the Far East.</div>'
+        ]
+    },
+    {
+        q:[
+            '<div>Resorts like Kuta and Sanur boomed with the advent of __________ in the late 20th century.</div>'
+        ],
+        a:[
+            '<div>Resorts like Kuta and Sanur boomed with the advent of <b><i>mass tourism</i></b> in the late 20th century.</div>'
+        ]
+    },
+    {
+        q:[
+            '<div>More __________ are needed to accommodate the needs of holiday-makers.</div>'
+        ],
+        a:[
+            '<div>More <b><i>tourist facilities</i></b> are needed to accommodate the needs of holiday-makers.</div>'
+        ]
+    },
+    {
+        q:[
+            '<div>Unless you are a citizen of the EU, you’ll need to __________ for a __________.</div>'
+        ],
+        a:[
+            '<div>Unless you are a citizen of the EU, you’ll need to <b><i>apply</i></b> for a <b><i>visa</i></b>.</div>'
+        ]
+    },
+    {
+        q:[
+            '<div>The city sky-line has been transformed with the construction of dozens of __________ hotels.</div>'
+        ],
+        a:[
+            '<div>The city sky-line has been transformed with the construction of dozens of <b><i>high-rise</i></b> hotels.</div>'
+        ]
+    },
+    {
+        q:[
+            '<div>__________ tend to spend less money than older, more conventional tourists.</div>'
+        ],
+        a:[
+            '<div><b><i>Back-packers</i></b> tend to spend less money than older, more conventional tourists.</div>'
+        ]
+    },
+    {
+        q:[
+            '<div>It can be hard to keep kids occupied on a __________ flight, like the one from London to Tokyo.</div>'
+        ],
+        a:[
+            '<div>It can be hard to keep kids occupied on a <b><i>long-haul</i></b> flight, like the one from London to Tokyo.</div>'
+        ]
+    }
+]
+
+</script>
+
+<Multiplayer title="Tourism and Travel" qa={qa} />
