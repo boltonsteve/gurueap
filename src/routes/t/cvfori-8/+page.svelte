cb87dc9bc6aa81d671500000d53d9b211625ac26
stevebolton <sgbolton@gmail.com>
2024-08-20 07:22:12 +0800

    multiplayer vocab 8 B

--- src/routes/t/cvfori-8/+page.svelte
+++ src/routes/t/cvfori-8/+page.svelte
@@ -5,162 +5,162 @@ import Multiplayer from '../../components/Multiplayer.svelte'
 let qa = [
     {
         q: [
-            '<div>During the Colonial_Era, Indonesia was occupied by the Dutch.</div>'
+            '<div>During the C__________ E__________, Indonesia was occupied by the Dutch.</div>'
         ],
         a: [
-            '<div>During the Colonial_Era, Indonesia was occupied by the Dutch.</div>'
+            '<div>During the <b><i>Colonial Era</i></b>, Indonesia was occupied by the Dutch.</div>'
         ]
     },
     {
         q: [
-            '<div>Archeological_evidence suggests the city of Damascus was founded six thousand years ago.</div>'
+            '<div>A__________ e__________ suggests the city of Damascus was founded six thousand years ago.</div>'
         ],
         a: [
-            '<div>Archeological_evidence suggests the city of Damascus was founded six thousand years ago.</div>'
+            '<div><b><i>Archeological evidence</i></b> suggests the city of Damascus was founded six thousand years ago.</div>'
         ]
     },
     {
         q: [
-            '<div>The police organised the cases in chronological_order.</div>'
+            '<div>The police organised the cases in c_________ o_________.</div>'
         ],
         a: [
-            '<div>The police organised the cases in chronological_order.</div>'
+            '<div>The police organised the cases in <b><i>chronological order</i></b>.</div>'
         ]
     },
     {
         q: [
-            '<div>In the_Middle_Ages, the only large buildings were castles and cathedrals.</div>'
+            '<div>In the M__________ A__________, the only large buildings were castles and cathedrals.</div>'
         ],
         a: [
-            '<div>In the_Middle_Ages, the only large buildings were castles and cathedrals.</div>'
+            '<div>In the <b><i>Middle Ages</i></b>, the only large buildings were castles and cathedrals.</div>'
         ]
     },
     {
         q: [
-            '<div>In the late_twentieth_century, many communist countries turned to capitalism.</div>'
+            '<div>In the late t__________ c__________, many communist countries turned to capitalism.</div>'
         ],
         a: [
-            '<div>In the late_twentieth_century, many communist countries turned to capitalism.</div>'
+            '<div>In the late <b><i>twentieth century</i></b>, many communist countries turned to capitalism.</div>'
         ]
     },
     {
         q: [
-            '<div>In recent_years, China has become the world’s most dynamic economy.</div>'
+            '<div>In r__________ y__________, China has become the world’s most dynamic economy.</div>'
         ],
         a: [
-            '<div>In recent_years, China has become the world’s most dynamic economy.</div>'
+            '<div>In <b><i>recent years</i></b>, China has become the world’s most dynamic economy.</div>'
         ]
     },
     {
         q: [
-            '<div>Prehistoric_remains/ruins, including stone circles and graves, can be found in many parts of the UK.</div>'
+            '<div>P_________ r__________/r__________ including stone circles and graves, can be found in many parts of the UK.</div>'
         ],
         a: [
-            '<div>Prehistoric_remains/ruins, including stone circles and graves, can be found in many parts of the UK.</div>'
+            '<div><b><i>Prehistoric remains/ruins</i></b>, including stone circles and graves, can be found in many parts of the UK.</div>'
         ]
     },
     {
         q: [
-            '<div>The Olympic Games were first held during classical_times.</div>'
+            '<div>The Olympic Games were first held during c__________ __________s.</div>'
         ],
         a: [
-            '<div>The Olympic Games were first held during classical_times.</div>'
+            '<div>The Olympic Games were first held during <b><i>classical times</i></b>.</div>'
         ]
     },
     {
         q: [
-            '<div>Long_ago, sailors from Sumatra crossed the Indian Ocean and settled in Madagascar.</div>'
+            '<div>__________ a__________, sailors from Sumatra crossed the Indian Ocean and settled in Madagascar.</div>'
         ],
         a: [
-            '<div>Long_ago, sailors from Sumatra crossed the Indian Ocean and settled in Madagascar.</div>'
+            '<div><b><i>Long ago</i></b>, sailors from Sumatra crossed the Indian Ocean and settled in Madagascar.</div>'
         ]
     },
     {
         q: [
-            '<div>Despite the comforts of the modern world, people often feel nostalgic for the past.</div>'
+            '<div>Despite the comforts of the modern world, people often feel n__________ for the past.</div>'
         ],
         a: [
-            '<div>Despite the comforts of the modern world, people often feel nostalgic for the past.</div>'
+            '<div>Despite the comforts of the modern world, people often feel <b><i>nostalgic</i></b> for the past.</div>'
         ]
     },
     {
         q: [
-            '<div>In the post-9/11_years, there has been a greater focus on soft diplomacy.</div>'
+            '<div>In the p__________-9/11 __________s, there has been a greater focus on soft diplomacy.</div>'
         ],
         a: [
-            '<div>In the post-9/11_years, there has been a greater focus on soft diplomacy.</div>'
+            '<div>In the <b><i>post-9/11 years</i></b>, there has been a greater focus on soft diplomacy.</div>'
         ]
     },
     {
         q: [
-            '<div>The early_twentieth_century witnessed the emergence of mass culture, including cinema and popular music.</div>'
+            '<div>The early t__________ c__________ witnessed the emergence of mass culture, including cinema and popular music.</div>'
         ],
         a: [
-            '<div>The early_twentieth_century witnessed the emergence of mass culture, including cinema and popular music.</div>'
+            '<div>The early <b><i>twentieth century</i></b> witnessed the emergence of mass culture, including cinema and popular music.</div>'
         ]
     },
     {
         q: [
-            '<div>During the Cold_War years, the USSR and the USA confronted each other with the threat of mutually assured destruction.</div>'
+            '<div>During the C__________ __________s, the USSR and the USA confronted each other with the threat of mutually assured destruction.</div>'
         ],
         a: [
-            '<div>During the Cold_War years, the USSR and the USA confronted each other with the threat of mutually assured destruction.</div>'
+            '<div>During the <b><i>Cold War</i></b> years, the USSR and the USA confronted each other with the threat of mutually assured destruction.</div>'
         ]
     },
     {
         q: [
-            '<div>The UK is often viewed as the birthplace of the Industrial_Revolution.</div>'
+            '<div>The UK is often viewed as the birthplace of the I__________ R__________.</div>'
         ],
         a: [
-            '<div>The UK is often viewed as the birthplace of the Industrial_Revolution.</div>'
+            '<div>The UK is often viewed as the birthplace of the <b><i>Industrial Revolution</i></b>.</div>'
         ]
     },
     {
         q: [
-            '<div>Because of the advent of the Internet, the current era is sometimes known as the Information_Age.</div>'
+            '<div>Because of the advent of the Internet, the current era is sometimes known as the I__________ A__________.</div>'
         ],
         a: [
-            '<div>Because of the advent of the Internet, the current era is sometimes known as the Information_Age.</div>'
+            '<div>Because of the advent of the Internet, the current era is sometimes known as the <b><i>Information Age</i></b>.</div>'
         ]
     },
     {
         q: [
-            '<div>The New_Order came to an end when the Asian Financial Crisis struck in 1997-98.</div>'
+            '<div>The N__________ O__________ came to an end when the Asian Financial Crisis struck in 1997-98.</div>'
         ],
         a: [
-            '<div>The New_Order came to an end when the Asian Financial Crisis struck in 1997-98.</div>'
+            '<div>The <b><i>New Order</i></b> came to an end when the Asian Financial Crisis struck in 1997-98.</div>'
         ]
     },
     {
         q: [
-            '<div>Central Java is full of ancient_monuments, such as Prambanan and Borobudur.</div>'
+            '<div>Central Java is full of a__________ m__________, such as Prambanan and Borobudur.</div>'
         ],
         a: [
-            '<div>Central Java is full of ancient_monuments, such as Prambanan and Borobudur.</div>'
+            '<div>Central Java is full of <b><i>ancient monuments</i></b>, such as Prambanan and Borobudur.</div>'
         ]
     },
     {
         q: [
-            '<div>The voyages of Columbus, Magellan and Vasco da Gama heralded the Age_of_Discovery.</div>'
+            '<div>The voyages of Columbus, Magellan and Vasco da Gama heralded the A__________ of D__________.</div>'
         ],
         a: [
-            '<div>The voyages of Columbus, Magellan and Vasco da Gama heralded the Age_of_Discovery.</div>'
+            '<div>The voyages of Columbus, Magellan and Vasco da Gama heralded the <b><i>Age of Discovery</i></b>.</div>'
         ]
     },
     {
         q: [
-            '<div>Since_independence, Indonesia’s population has multiplied several times.</div>'
+            '<div>S__________ i__________, Indonesia’s population has multiplied several times.</div>'
         ],
         a: [
-            '<div>Since_independence, Indonesia’s population has multiplied several times.</div>'
+            '<div><b><i>Since independence</i></b>, Indonesia’s population has multiplied several times.</div>'
         ]
     },
     {
         q: [
-            '<div>Should old buildings be preserved for posterity or should they simply be knocked down?</div>'
+            '<div>Should old buildings be preserved for p__________ or should they simply be knocked down?</div>'
         ],
         a: [
-            '<div>Should old buildings be preserved for posterity or should they simply be knocked down?</div>'
+            '<div>Should old buildings be preserved for <b><i>posterity</i></b> or should they simply be knocked down?</div>'
         ]
     }
 ]
