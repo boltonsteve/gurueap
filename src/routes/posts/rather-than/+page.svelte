8ccb88095d33436f63b92e646918e8b8c97972fc
stevebolton <sgbolton@gmail.com>
2025-02-20 08:09:26 +0800

    update rather like 2

--- src/routes/posts/rather-than/+page.svelte
+++ src/routes/posts/rather-than/+page.svelte
@@ -29,12 +29,23 @@
         <li>Modern people are not like ancient people.</li>
     </ol>
 
-    <h2>Rather (than)</h2>
-    <p>Try to restrict your use of <b><i>rather</i></b> to situations in which you express a preference:</p>
+    <h2>Rather (than) #1</h2>
+    <p>You can use <b><i>rather</i></b> in situations in which you express a preference:</p>
 
     <div class="alert alert-success font-italic">
         <p class="font-italic"><span class="font-weight-bold text-success">Rather than</span> spend 2 hours a day stuck in traffic, I would prefer to stay at home.</p>
         <p class="font-italic">Being stuck in traffic is no fun. I<span class="font-weight-bold text-success">'d rather</span> stay at home and watch a movie on Netflix.</p>
     </div>
+
+    <h2>Rather (than) #2</h2>
+    <p>You can also use rather to show that something belongs more to one category and less to another:</p>
+
+    <div class="alert alert-success font-italic">
+        <p class="font-italic">This book is journalism <span class="font-weight-bold text-success">rather than</span> science.</p>
+        <p class="font-italic">The best security policies are proactive <span class="font-weight-bold text-success">rather than</span> reactive.</p>
+        <p class="font-italic">Use body wash <span class="font-weight-bold text-success">rather than</span> bar soap.</p>
+    </div>
+
+    <p>See <a href="https://skell.sketchengine.eu/#result?lang=en&query=rather%20than&f=concordance" target="_blank">here</a> for more examples.</p>
 
 </div>
