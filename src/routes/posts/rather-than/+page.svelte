eaef84cc0ce2e047393a2f8c1d04b0feae0d3fc6
stevebolton <sgbolton@gmail.com>
2025-02-20 08:01:28 +0800

    update rather like

--- src/routes/posts/rather-than/+page.svelte
+++ src/routes/posts/rather-than/+page.svelte
@@ -5,18 +5,20 @@
 </script>
 
 <div class="mb-3 mw-500">
+
+    <img src="/img/scales.png" class="img-fluid mx-auto d-block" alt="scales">
     
     <h1>Rather than</h1>
 
     <div class="alert alert-danger font-italic">Most people currently suffer from anxiety <span class="text-danger font-weight-bold">rather than</span> ancient people in the past.</div>
 
-    <p>Literally this means</p>
+    <p>Literally this means:</p>
     <ol>
         <li>Modern people suffer from anxiety.</li>
         <li>Modern people do not suffer from ancient people.</li>
     </ol>
 
-    <p>First of all I think our writer means to use <b><i>unlike</i></b>:</p>
+    <p>First of all I think our writer would be better off using <b><i>unlike</i></b>:</p>
 
     <div class="alert alert-success font-italic">Most modern people suffer from anxiety, <span class="text-success font-weight-bold">unlike</span> ancient people in the past.</div>
 
@@ -26,5 +28,13 @@
         <li>Ancient people in the past did not suffer from anxiety.</li>
         <li>Modern people are not like ancient people.</li>
     </ol>
+
+    <h2>Rather (than)</h2>
+    <p>Try to restrict your use of <b><i>rather</i></b> to situations in which you express a preference:</p>
+
+    <div class="alert alert-success font-italic">
+        <p class="font-italic"><span class="font-weight-bold text-success">Rather than</span> spend 2 hours a day stuck in traffic, I would prefer to stay at home.</p>
+        <p class="font-italic">Being stuck in traffic is no fun. I<span class="font-weight-bold text-success">'d rather</span> stay at home and watch a movie on Netflix.</p>
+    </div>
 
 </div>
